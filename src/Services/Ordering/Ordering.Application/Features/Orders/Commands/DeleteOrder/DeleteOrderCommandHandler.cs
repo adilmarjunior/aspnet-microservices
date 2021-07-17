b3c28e6a530f35daf46b8f040b116549be47b1d9
Adilmar Junior <adilmar.junior@rockcontent.com>
2021-07-17 11:45:00 -0300

    create application exceptions

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -5,6 +5,8 @@ using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
 {
@@ -30,8 +32,7 @@ namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
 
       if(orderToDelete == null)
       {
-        _logger.LogError("Order not found");
-        //throw new NotFoundException(nameof(Order), request.Id);
+        throw new NotFoundException(nameof(Order), request.Id);
       }
       
       await _orderRepository.DeleteAsync(orderToDelete);
